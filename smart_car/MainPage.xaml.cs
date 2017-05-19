27fddf21889793665c04d53c03782848454dce08
kuchiki111 <394957249@qq.com>
2017-05-19 09:43:08 +0800

    2017.05.19 09:45 first push

--- smart_car/MainPage.xaml.cs
+++ smart_car/MainPage.xaml.cs
@@ -12,19 +12,67 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+using Windows.Devices.Gpio;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace smart_car
 {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
+        myGpioPin mypin = new myGpioPin();
+
         public MainPage()
         {
             this.InitializeComponent();
+
         }
+
+
+        public void init()
+        {
+            GpioController gpio = GpioController.GetDefault();
+
+            mypin.pin5 = gpio.OpenPin(7);
+            mypin.pin6 = gpio.OpenPin(11);
+            mypin.pin23 = gpio.OpenPin(13);
+            mypin.pin24 = gpio.OpenPin(15);
+            mypin.pin17 = gpio.OpenPin(16);
+            mypin.pin27 = gpio.OpenPin(18);
+
+            mypin.pin5.SetDriveMode(GpioPinDriveMode.Output);
+            mypin.pin6.SetDriveMode(GpioPinDriveMode.Output);
+            mypin.pin23.SetDriveMode(GpioPinDriveMode.Output);
+            mypin.pin24.SetDriveMode(GpioPinDriveMode.Output);
+            mypin.pin17.SetDriveMode(GpioPinDriveMode.Output);
+            mypin.pin27.SetDriveMode(GpioPinDriveMode.Output);
+
+        }
+
+        public void forword(int time)
+        {
+            init();
+
+            mypin.pin5.Write(GpioPinValue.High);
+            mypin.pin6.Write(GpioPinValue.Low);
+            mypin.pin5.Write(GpioPinValue.High);
+            mypin.pin6.Write(GpioPinValue.Low);
+
+            Task.Delay(time);
+
+        }
+
+
+    }
+
+    class myGpioPin
+    {
+        public GpioPin pin5;
+        public GpioPin pin6;
+        public GpioPin pin23;
+        public GpioPin pin24;
+        public GpioPin pin17;
+        public GpioPin pin27;
     }
 }
---
+++ smart_car/PWM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace smart_car
+{
+    class PWM
+    {
+        public GpioPin channel;
+        public double frequency;
+
+        public PWM(GpioPin channel, double frequency)
+        {
+            this.channel = channel;
+            this.frequency = frequency;
+        }
+
+        public void start(double dc)
+        {
+
+        }
+
+        public void ChangeFrequency(double frequency)
+        {
+
+        }
+
+        public void ChangeDutyCycle(double dc)
+        {
+
+        }
+
+        public void stop()
+        {
+
+        }
+
+
+    }
+}
